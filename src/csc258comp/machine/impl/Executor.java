fad83427c1ca4655256ef7005ad00b3da491375a
Nayuki Minase <nayuki@eigenstate.org>
2010-08-13 13:24:51 -0400

    Clarified and cleaned up executor's code.

--- src/csc258comp/machine/impl/Executor.java
+++ src/csc258comp/machine/impl/Executor.java
@@ -9,64 +9,61 @@ import csc258comp.machine.model.MachineState;
 
 public final class Executor {
 	
+	/**
+	 * Executes one instruction on the specified machine.
+	 * @param m the machine to execute on
+	 * @throws IllegalStateException if an invalid opcode is encountered
+	 * @throws IOException if an I/O exception occurs
+	 */
 	public static void step(Machine m) throws IOException {
 		MachineState s = m.getState();
 		
+		// Do nothing if halted
 		if (s.isHalted())
 			return;
 		
-		int instAddr = s.getProgramCounter();
-		if (instAddr == Program.OPSYS_ADDRESS) {
+		// Check if halting
+		int pc = s.getProgramCounter();
+		if (pc == Program.OPSYS_ADDRESS) {
 			s.setHalted(true);
 			return;
 		}
 		
-		int word = s.getMemoryAt(instAddr);
-		int oper = word >>> 24;
-		int addr = word & ((1 << 24) - 1);
-		int nextInstAddr;
+		// Fetch instruction
+		int instWord = s.getMemoryAt(pc);
+		int op = instWord >>> 24;
+		int memAddr = instWord & ((1 << 24) - 1);  // The memory address embedded in the instruction word
+		int nextPc = -1;  // -1 is the default value that signifies that nextPc should be pc + 1
 		
-		if (oper == 0) {  // LDA
-			s.setAccumulator(s.getMemoryAt(addr));
-			nextInstAddr = instAddr + 1;
-				
-		} else if (oper == 1) {  // STA
-			s.setMemoryAt(addr, s.getAccumulator());
-			nextInstAddr = instAddr + 1;
-				
-		} else if (oper >= 2 && oper <= 6 || oper >= 13 && oper <= 15) {  // ADD, SUB, MUL, DIV, MOD, AND, IOR, XOR
+		// Execute instruction
+		if (op == 0) {  // LDA
+			s.setAccumulator(s.getMemoryAt(memAddr));
+			
+		} else if (op == 1) {  // STA
+			s.setMemoryAt(memAddr, s.getAccumulator());
+			
+		} else if (op >= 2 && op <= 6 || op >= 13 && op <= 15) {  // ADD, SUB, MUL, DIV, MOD, AND, IOR, XOR
 			int x = s.getAccumulator();
-			int y = s.getMemoryAt(addr);
+			int y = s.getMemoryAt(memAddr);
 			int result;
-			switch (oper) {
-				case  2:  result = x + y;  break;
-				case  3:  result = x - y;  break;
-				case  4:  result = x * y;  break;
-				case  5:  result = (y != 0 ? x / y : x);  break;
-				case  6:  result = (y != 0 ? x % y : x);  break;
-				case 13:  result = x & y;  break;
-				case 14:  result = x | y;  break;
-				case 15:  result = x ^ y;  break;
+			switch (op) {
+				case  2:  result = x + y;  s.setConditionCode(result != (long)x + y);  break;
+				case  3:  result = x - y;  s.setConditionCode(result != (long)x - y);  break;
+				case  4:  result = x * y;  s.setConditionCode(result != (long)x * y);  break;
+				case  5:  result = (y != 0 ? x / y : x);  s.setConditionCode(y == 0);  break;
+				case  6:  result = (y != 0 ? x % y : x);  s.setConditionCode(y == 0);  break;
+				case 13:  result = x & y;  s.setConditionCode(result != 0);  break;
+				case 14:  result = x | y;  s.setConditionCode(result != 0);  break;
+				case 15:  result = x ^ y;  s.setConditionCode(result != 0);  break;
 				default:  throw new AssertionError();
 			}
 			s.setAccumulator(result);
-			if (oper == 2)
-				s.setConditionCode(result != (long)x + y);
-			else if (oper == 3)
-				s.setConditionCode(result != (long)x - y);
-			else if (oper == 4)
-				s.setConditionCode(result != (long)x * y);
-			else if (oper == 5 || oper == 6)
-				s.setConditionCode(y == 0);
-			else
-				s.setConditionCode(result != 0);
-			nextInstAddr = instAddr + 1;
-				
-		} else if (oper >= 7 && oper <= 10) {  // FLA, FLS, FLM, FLD
+			
+		} else if (op >= 7 && op <= 10) {  // FLA, FLS, FLM, FLD
 			float x = Float.intBitsToFloat(s.getAccumulator());
-			float y = Float.intBitsToFloat(s.getMemoryAt(addr));
+			float y = Float.intBitsToFloat(s.getMemoryAt(memAddr));
 			float result;
-			switch (oper) {
+			switch (op) {
 				case  7:  result = x + y;  break;
 				case  8:  result = x - y;  break;
 				case  9:  result = x * y;  break;
@@ -74,52 +71,52 @@ public final class Executor {
 				default:  throw new AssertionError();
 			}
 			s.setAccumulator(Float.floatToRawIntBits(result));
-			s.setConditionCode(Float.isInfinite(result));
-			if (oper >= 7 && oper <= 9)
-				s.setConditionCode(false);  // Overflow detection not implemented
-			else if (y == 0)
-				s.setConditionCode(true);
-			nextInstAddr = instAddr + 1;
+			s.setConditionCode(Float.isInfinite(result) || op == 10 && y == 0);
 			
-		} else if (oper == 11) {  // CIF
-			s.setAccumulator(Float.floatToRawIntBits((float)s.getMemoryAt(addr)));
-			nextInstAddr = instAddr + 1;
+		} else if (op == 11) {  // CIF
+			int ival = s.getMemoryAt(memAddr);
+			float fval = (float)ival;
+			int bits = Float.floatToRawIntBits(fval);
+			s.setAccumulator(bits);
 			
-		} else if (oper == 12) {  // CFI
-			s.setAccumulator((int)Math.round(Float.intBitsToFloat(s.getMemoryAt(addr))));
-			nextInstAddr = instAddr + 1;
+		} else if (op == 12) {  // CFI
+			int bits = s.getMemoryAt(memAddr);
+			float fval = Float.intBitsToFloat(bits);
+			int ival = (int)Math.round(fval);
+			s.setAccumulator(ival);
 			
-		} else if (oper >= 16 && oper <= 19) {  // BUN, BZE, BSA, BIN
-			switch (oper) {
-				case 16:  nextInstAddr = addr;  break;
-				case 17:  nextInstAddr = (s.getConditionCode() ? instAddr + 1 : addr);  break;
-				case 18:  s.setMemoryAt(addr, instAddr + 1);  nextInstAddr = addr + 1;  break;
-				case 19:  nextInstAddr = s.getMemoryAt(addr);  break;
+		} else if (op >= 16 && op <= 19) {  // BUN, BZE, BSA, BIN
+			switch (op) {
+				case 16:  nextPc = memAddr;  break;
+				case 17:  nextPc = (s.getConditionCode() ? pc + 1 : memAddr);  break;
+				case 18:  s.setMemoryAt(memAddr, pc + 1);  nextPc = memAddr + 1;  break;
+				case 19:  nextPc = s.getMemoryAt(memAddr);  break;
 				default:  throw new AssertionError();
 			}
 			
-		} else if (oper == 20) {  // INP
+		} else if (op == 20) {  // INP
 			int c = m.input();
-			if (c != -1) {
+			if (c != -1)
 				s.setAccumulator(c);
-				nextInstAddr = instAddr + 1;
-			} else {  // EOF reached
-				nextInstAddr = addr;
-			}
+			else
+				nextPc = memAddr;
 			
-		} else if (oper == 21) {  // OUT
-			if (m.output(s.getAccumulator() & 0xFF)) {
-				nextInstAddr = instAddr + 1;
-			} else {
-				nextInstAddr = addr;
-			}
+		} else if (op == 21) {  // OUT
+			if (m.output(s.getAccumulator() & 0xFF))
+				;  // Success, do nothing, go to next instruction
+			else
+				nextPc = memAddr;
 			
-		} else {
+		} else {  // Illegal
 			throw new IllegalStateException("Illegal opcode");
 		}
 		
-		s.setProgramCounter(nextInstAddr);
+		// Set next program counter
+		if (nextPc == -1)
+			nextPc = pc + 1;
+		s.setProgramCounter(nextPc);
 	}
+	
 	
 	
 	private Executor() {}
