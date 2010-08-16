ff28c17889700e62878cb0e48adefa1994f7452b
Nayuki Minase <nayuki@eigenstate.org>
2010-08-16 23:30:21 +0000

    (Broken) Added Utf8Reader and made use of it.

--- src/csc258comp/compiler/SourceCode.java
+++ src/csc258comp/compiler/SourceCode.java
@@ -2,20 +2,20 @@ package csc258comp.compiler;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+
+import csc258comp.util.Utf8Reader;
 
 
 public final class SourceCode implements Iterable<String> {
 	
 	public static SourceCode readFile(File file) throws IOException {
 		List<String> lines = new ArrayList<String>();
-		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+		BufferedReader in = new Utf8Reader(file);
 		try {
 			while (true) {
 				String line = in.readLine();
