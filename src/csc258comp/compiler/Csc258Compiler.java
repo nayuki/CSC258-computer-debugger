58c3148b06f09f2bce0723e8f2d8cc43992b4ee2
Nayuki Minase <nayuki@eigenstate.org>
2010-08-18 04:06:58 +0000

    Refactored and clarified compiler.

--- src/csc258comp/compiler/Csc258Compiler.java
+++ src/csc258comp/compiler/Csc258Compiler.java
@@ -36,20 +36,12 @@ public final class Csc258Compiler {
 	
 	
 	private Csc258Compiler(SourceCode source) throws CompilationException {
+		// Loop over source code lines
 		for (int i = 0; i < source.getLineCount(); i++) {
 			Tokenizer t = new Tokenizer(source.getLineAt(i));
 			
 			// Consume labels
-			while (true) {
-				String label = t.nextLabel();
-				if (label == null)
-					break;
-				
-				if (!labels.containsKey(label))
-					labels.put(label, image.length());
-				else
-					errorMessages.put(i, String.format("Duplicate label \"%s\"", label));
-			}
+			processLabels(t, i);
 			
 			if (t.isEmpty())
 				continue;
@@ -61,119 +53,12 @@ public final class Csc258Compiler {
 			}
 			mnemonic = mnemonic.toUpperCase();
 			
-			// Instruction word
-			if (InstructionSet.getOpcodeIndex(mnemonic) != -1) {
-				String ref = t.nextReference();
-				if (ref == null) {
-					errorMessages.put(i, "Reference expected");
-					continue;
-				}
-				references.put(image.length(), ref);
-				
-				int word = InstructionSet.getOpcodeIndex(mnemonic) << 24;
-				appendWord(word, i);
-			}
-			// Data word
-			else if (mnemonic.length() == 1 && "IFCBHAW".indexOf(mnemonic) != -1) {
-				String val = null;
-				if ("IFBHW".indexOf(mnemonic) != -1) {
-					val = t.nextToken();
-					if (val == null) {
-						errorMessages.put(i, String.format("String expected", val));
-						continue;
-					}
-				}
-				
-				switch (mnemonic.charAt(0)) {
-					case 'I':
-						try {
-							appendWord(Integer.parseInt(val), i);
-						} catch (NumberFormatException e) {
-							errorMessages.put(i, String.format("Invalid integer value \"%s\"", val));
-						}
-						break;
-						
-					case 'F':
-						try {
-							appendWord(Float.floatToRawIntBits(Float.parseFloat(val)), i);
-						} catch (NumberFormatException e) {
-							errorMessages.put(i, String.format("Invalid floating-point value \"%s\"", val));
-						}
-						break;
-						
-					case 'C':
-						val = t.nextString();
-						if (val == null) {
-							errorMessages.put(i, "String expected");
-							continue;
-						}
-						try {
-							appendWord(parseChars(val), i);
-						} catch (IllegalArgumentException e) {
-							errorMessages.put(i, e.getMessage());
-						}
-						
-						break;
-						
-					case 'B':
-						try {
-							long binval = Long.parseLong(val, 2);
-							if (binval >= 0 && binval <= 0xFFFFFFFFL) {
-								appendWord((int)binval, i);
-							} else {
-								errorMessages.put(i, "Binary value out of range");
-							}
-						} catch (NumberFormatException e) {
-							errorMessages.put(i, "Invalid binary value");
-						}
-						break;
-						
-					case 'H':
-						try {
-							long hexval = Long.parseLong(val, 16);
-							if (hexval >= 0 && hexval <= 0xFFFFFFFFL) {
-								appendWord((int)hexval, i);
-							} else {
-								errorMessages.put(i, "Hexadecimal value out of range");
-							}
-						} catch (NumberFormatException e) {
-							errorMessages.put(i, "Invalid hexadecimal value");
-						}
-						break;
-						
-					case 'A':
-						val = t.nextReference();
-						if (val == null) {
-							errorMessages.put(i, "Reference expected");
-							continue;
-						}
-						references.put(image.length(), val);
-						appendWord(0, i);
-						break;
-						
-					case 'W':
-						try {
-							int length = Integer.parseInt(val);
-							if (length >= 0 && length <= (1 << 24)) {
-								if (length > 0) {
-									appendWord(0, i);
-									if (length > 1)
-										image.append(new int[length - 1]);
-								}
-							} else if (length < 0) {
-								errorMessages.put(i, "Negative size");
-							} else {
-								errorMessages.put(i, "Size out of range");
-							}
-						} catch (NumberFormatException e) {
-							errorMessages.put(i, "Invalid size");
-						}
-						break;
-				}
-				
-			} else {
+			if (InstructionSet.getOpcodeIndex(mnemonic) != -1)  // Instruction word
+				processInstructionWord(t, mnemonic, i);
+			else if (mnemonic.length() == 1 && "IFCBHAW".indexOf(mnemonic) != -1)  // Data word
+				processDataWord(t, mnemonic.charAt(0), i);
+			else  // Invalid mnemonic
 				errorMessages.put(i, String.format("Invalid mnemonic \"%s\"", mnemonic));
-			}
 			
 			// Ignore the rest of the line, which is treated as a comment
 		}
@@ -182,6 +67,135 @@ public final class Csc258Compiler {
 			throw new CompilationException(String.format("%d compiler errors", errorMessages.size()), errorMessages, source);
 		
 		result = new Fragment(image.toArray(), labels, references, source, srcLineToAddr, addrToSrcLine);
+	}
+
+
+	private void processLabels(Tokenizer t, int lineNum) {
+		while (true) {
+			String label = t.nextLabel();
+			if (label == null)
+				break;
+			
+			if (!labels.containsKey(label))
+				labels.put(label, image.length());
+			else
+				errorMessages.put(lineNum, String.format("Duplicate label \"%s\"", label));
+		}
+	}
+	
+	
+	private void processInstructionWord(Tokenizer t, String mnemonic, int lineNum) {
+		String ref = t.nextReference();
+		if (ref == null) {
+			errorMessages.put(lineNum, "Reference expected");
+			return;
+		}
+		
+		references.put(image.length(), ref);
+		int word = InstructionSet.getOpcodeIndex(mnemonic) << 24;
+		appendWord(word, lineNum);
+	}
+	
+	
+	private void processDataWord(Tokenizer t, char mnemonic, int i) {
+		String val = null;
+		if ("IFBHW".indexOf(mnemonic) != -1) {
+			val = t.nextToken();
+			if (val == null) {
+				errorMessages.put(i, String.format("String expected", val));
+				return;
+			}
+		}
+		
+		switch (mnemonic) {
+			case 'I':
+				try {
+					appendWord(Integer.parseInt(val), i);
+				} catch (NumberFormatException e) {
+					errorMessages.put(i, String.format("Invalid integer value \"%s\"", val));
+				}
+				break;
+				
+			case 'F':
+				try {
+					appendWord(Float.floatToRawIntBits(Float.parseFloat(val)), i);
+				} catch (NumberFormatException e) {
+					errorMessages.put(i, String.format("Invalid floating-point value \"%s\"", val));
+				}
+				break;
+				
+			case 'C':
+				val = t.nextString();
+				if (val == null) {
+					errorMessages.put(i, "String expected");
+					return;
+				}
+				try {
+					appendWord(parseChars(val), i);
+				} catch (IllegalArgumentException e) {
+					errorMessages.put(i, e.getMessage());
+				}
+				
+				break;
+				
+			case 'B':
+				try {
+					long binval = Long.parseLong(val, 2);
+					if (binval >= 0 && binval <= 0xFFFFFFFFL) {
+						appendWord((int)binval, i);
+					} else {
+						errorMessages.put(i, "Binary value out of range");
+					}
+				} catch (NumberFormatException e) {
+					errorMessages.put(i, "Invalid binary value");
+				}
+				break;
+				
+			case 'H':
+				try {
+					long hexval = Long.parseLong(val, 16);
+					if (hexval >= 0 && hexval <= 0xFFFFFFFFL) {
+						appendWord((int)hexval, i);
+					} else {
+						errorMessages.put(i, "Hexadecimal value out of range");
+					}
+				} catch (NumberFormatException e) {
+					errorMessages.put(i, "Invalid hexadecimal value");
+				}
+				break;
+				
+			case 'A':
+				val = t.nextReference();
+				if (val == null) {
+					errorMessages.put(i, "Reference expected");
+					return;
+				}
+				references.put(image.length(), val);
+				appendWord(0, i);
+				break;
+				
+			case 'W':
+				try {
+					int length = Integer.parseInt(val);
+					if (length >= 0 && length <= (1 << 24)) {
+						if (length > 0) {
+							appendWord(0, i);
+							if (length > 1)
+								image.append(new int[length - 1]);
+						}
+					} else if (length < 0) {
+						errorMessages.put(i, "Negative size");
+					} else {
+						errorMessages.put(i, "Size out of range");
+					}
+				} catch (NumberFormatException e) {
+					errorMessages.put(i, "Invalid size");
+				}
+				break;
+			
+			default:
+				throw new AssertionError();
+		}
 	}
 	
 	
