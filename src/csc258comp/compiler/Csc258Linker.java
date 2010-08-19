7d6f5c6e0cb06024c40917c21bc1cdfb36f0b520
Nayuki Minase <nayuki@eigenstate.org>
2010-08-19 03:44:37 +0000

    Reorganized linker without changing external behavior. Now the fragment layout can be customized.

--- src/csc258comp/compiler/Csc258Linker.java
+++ src/csc258comp/compiler/Csc258Linker.java
@@ -23,54 +23,73 @@ public final class Csc258Linker {
 	
 	
 	private Csc258Linker(Iterable<Fragment> frags) {
+		Map<Fragment,Integer> fragmentToOffset = layOutFragments(frags);
+		Map<String,Integer> allLabels = unionLabels(fragmentToOffset, frags);
+		int[] image = resolveAndBuildImage(frags, fragmentToOffset, allLabels);
+		
+		// Process debugging info
+		Map<SourceLine,Integer> srcLineToAddr = new HashMap<SourceLine,Integer>();
+		Map<Integer,SourceLine> addrToSrcLine = new HashMap<Integer,SourceLine>();
+		for (Fragment f : frags) {
+			SourceCode src = f.getSourceCode();
+			int off = fragmentToOffset.get(f);
+			
+			Map<Integer,Integer> aBySl = f.getAddressBySourceLineMap();
+			for (int l : aBySl.keySet()) {
+				srcLineToAddr.put(new SourceLine(src, l), aBySl.get(l) + off);
+			}
+			
+			Map<Integer,Integer> slByA = f.getSourceLineByAddressMap();
+			for (int a : slByA.keySet()) {
+				addrToSrcLine.put(a + off, new SourceLine(src, slByA.get(a)));
+			}
+		}
+		
+		result = new Program(image, allLabels.get("main"), srcLineToAddr, addrToSrcLine);
+	}
+	
+	
+	private static Map<Fragment,Integer> layOutFragments(Iterable<Fragment> frags) {
+		Map<Fragment,Integer> fragToOff = new HashMap<Fragment,Integer>();
+		int offset = 0;
+		for (Fragment f : frags) {
+			fragToOff.put(f, offset);
+			offset += f.getImageLength();
+		}
+		return fragToOff;
+	}
+	
+	
+	private static Map<String,Integer> unionLabels(Map<Fragment,Integer> fragToOff, Iterable<Fragment> frags) {
 		Map<String,Integer> alllabels = new HashMap<String,Integer>();
 		alllabels.put("opsys", Executor.OPSYS_ADDRESS);
 		
-		// Union all labels and relocate them
-		int offset = 0;
 		for (Fragment f : frags) {
+			int off = fragToOff.get(f);
 			Map<String,Integer> labels = f.getLabels();
 			for (String label : labels.keySet()) {
 				if (alllabels.containsKey(label))
 					throw new IllegalArgumentException(String.format("Duplicate label \"%s\"", label));
 				else
-					alllabels.put(label, labels.get(label) + offset);
+					alllabels.put(label, labels.get(label) + off);  // Does relocation
 			}
-			offset += f.getImageLength();
 		}
 		
-		// Resolve references and build program image
-		IntBuffer allimage = new IntBuffer();
-		for (Fragment f : frags) {
-			int[] image = resolveReferences(f, alllabels);
-			allimage.append(image);
-		}
-		
-		Map<SourceLine,Integer> srcLineToAddr = new HashMap<SourceLine,Integer>();
-		Map<Integer,SourceLine> addrToSrcLine = new HashMap<Integer,SourceLine>();
-		
-		offset = 0;
-		for (Fragment f : frags) {
-			SourceCode src = f.getSourceCode();
-			
-			Map<Integer,Integer> aBySl = f.getAddressBySourceLineMap();
-			for (int l : aBySl.keySet()) {
-				srcLineToAddr.put(new SourceLine(src, l), aBySl.get(l) + offset);
-			}
-			
-			Map<Integer,Integer> slByA = f.getSourceLineByAddressMap();
-			for (int a : slByA.keySet()) {
-				addrToSrcLine.put(a + offset, new SourceLine(src, slByA.get(a)));
-			}
-			
-			offset += f.getImageLength();
-		}
-		
-		result = new Program(allimage.toArray(), alllabels.get("main"), srcLineToAddr, addrToSrcLine);
+		return alllabels;
 	}
 	
 	
-	private static int[] resolveReferences(Fragment f, Map<String,Integer> alllabels) {
+	private static int[] resolveAndBuildImage(Iterable<Fragment> frags, Map<Fragment,Integer> fragToOff, Map<String,Integer> allLabels) {
+		IntBuffer allImage = new IntBuffer();
+		for (Fragment f : frags) {
+			int[] image = getImageAndResolveReferences(f, allLabels);
+			allImage.append(image);
+		}
+		return allImage.toArray();
+	}
+	
+	
+	private static int[] getImageAndResolveReferences(Fragment f, Map<String,Integer> alllabels) {
 		int[] image = f.getImage();
 		Map<Integer,String> refs = f.getReferences();
 		for (int addr : refs.keySet()) {
