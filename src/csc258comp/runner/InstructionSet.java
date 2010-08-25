4fd613d56a3d3269865205cda96b42064ff60c74
Nayuki Minase <nayuki@eigenstate.org>
2010-08-25 15:18:12 -0400

    Converted InstructionSet to use maps, thus allowing the possibility of discontiguous opcodes, multiple mnemonics, and such.

--- src/csc258comp/runner/InstructionSet.java
+++ src/csc258comp/runner/InstructionSet.java
@@ -1,5 +1,8 @@
 package csc258comp.runner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Contains information about the machine's instruction set and their mnemonics.
@@ -7,18 +10,47 @@ package csc258comp.runner;
  */
 public final class InstructionSet {
 	
-	/**
-	 * The list of mnemonics. Their element indices are directly used as opcodes.
-	 */
-	private static final String[] MNEMONICS = {
-		"LDA", "STA",
-		"ADD", "SUB", "MUL", "DIV", "MOD",
-		"FLA", "FLS", "FLM", "FLD",
-		"CIF", "CFI",
-		"AND", "IOR", "XOR",
-		"BUN", "BZE", "BSA", "BIN",
-		"INP", "OUT"
-	};
+	private static Map<Integer,String> opcodeToMnemonic;
+	
+	private static Map<String,Integer> mnemonicToOpcode;
+	
+	
+	static {
+		opcodeToMnemonic = new HashMap<Integer,String>();
+		mnemonicToOpcode = new HashMap<String,Integer>();
+		
+		addInstruction(0x00, "LDA");
+		addInstruction(0x01, "STA");
+		addInstruction(0x02, "ADD");
+		addInstruction(0x03, "SUB");
+		addInstruction(0x04, "MUL");
+		addInstruction(0x05, "DIV");
+		addInstruction(0x06, "MOD");
+		addInstruction(0x07, "FLA");
+		addInstruction(0x08, "FLS");
+		addInstruction(0x09, "FLM");
+		addInstruction(0x0A, "FLD");
+		addInstruction(0x0B, "CIF");
+		addInstruction(0x0C, "CFI");
+		addInstruction(0x0D, "AND");
+		addInstruction(0x0E, "IOR");
+		addInstruction(0x0F, "XOR");
+		addInstruction(0x10, "BUN");
+		addInstruction(0x11, "BZE");
+		addInstruction(0x12, "BSA");
+		addInstruction(0x13, "BIN");
+		addInstruction(0x14, "INP");
+		addInstruction(0x15, "OUT");
+	}
+	
+	
+	private static void addInstruction(int opcode, String mnemonic) {
+		if (!opcodeToMnemonic.containsKey(opcode))
+			opcodeToMnemonic.put(opcode, mnemonic);
+		
+		if (mnemonicToOpcode.put(mnemonic, opcode) != null)
+			throw new AssertionError("Attempting to map a mnemonic to multiple opcodes");
+	}
 	
 	
 	
@@ -28,11 +60,13 @@ public final class InstructionSet {
 	 * @return the mnemonic associated with the opcode, or {@code null} if none exist
 	 */
 	public static String getMnemonic(int opcode) {
-		if (opcode < 0)
+		if ((opcode & 0xFF) != opcode)
 			throw new IllegalArgumentException();
-		if (opcode >= MNEMONICS.length)
+		
+		if (opcodeToMnemonic.containsKey(opcode))
+			return opcodeToMnemonic.get(opcode);
+		else
 			return null;
-		return MNEMONICS[opcode];
 	}
 	
 	
@@ -42,14 +76,10 @@ public final class InstructionSet {
 	 * @return the opcode associated with the mnemonic, or {@code -1} if none exist
 	 */
 	public static int getOpcode(String mnemonic) {
-		if (mnemonic == null)
-			throw new NullPointerException();
-		// Uses linear search, which is good enough for parsing small programs
-		for (int i = 0; i < MNEMONICS.length; i++) {
-			if (mnemonic.equals(MNEMONICS[i]))
-				return i;
-		}
-		return -1;
+		if (mnemonicToOpcode.containsKey(mnemonic))
+			return mnemonicToOpcode.get(mnemonic);
+		else
+			return -1;
 	}
 	
 	
