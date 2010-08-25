a7b6cd8229175a30a554ce66b411550b42119566
Nayuki Minase <nayuki@eigenstate.org>
2010-08-25 15:34:24 -0400

    Made memory into a multi-level sparse array.

--- src/csc258comp/debugger/Memory.java
+++ src/csc258comp/debugger/Memory.java
@@ -3,14 +3,14 @@ package csc258comp.debugger;
 import csc258comp.runner.Machine;
 
 
-public final class Memory {
+final class Memory {
 	
-	private int[] values;
+	private int[][][] values;
 	
 	
 	
 	public Memory() {
-		values = new int[Machine.ADDRESS_SPACE_SIZE];
+		values = new int[1 << 8][][];
 	}
 	
 	
@@ -18,14 +18,40 @@ public final class Memory {
 	public int get(int addr) {
 		if (addr < 0 || addr >= Machine.ADDRESS_SPACE_SIZE)
 			throw new IllegalArgumentException("Address out of bounds");
-		return values[addr];
+		
+		int a0 = (addr >>> 16) & ((1 << 8) - 1);
+		int a1 = (addr >>>  8) & ((1 << 8) - 1);
+		int a2 = (addr >>>  0) & ((1 << 8) - 1);
+		
+		int[][] v0 = values[a0];
+		if (v0 == null)
+			values[a0] = v0 = new int[1 << 8][];
+		
+		int[] v1 = v0[a1];
+		if (v1 == null)
+			v0[a1] = v1 = new int[1 << 8];
+			
+		return v1[a2];
 	}
 	
 	
 	public void set(int addr, int val) {
 		if (addr < 0 || addr >= Machine.ADDRESS_SPACE_SIZE)
 			throw new IllegalArgumentException("Address out of bounds");
-		values[addr] = val;
+		
+		int a0 = (addr >>> 16) & ((1 << 8) - 1);
+		int a1 = (addr >>>  8) & ((1 << 8) - 1);
+		int a2 = (addr >>>  0) & ((1 << 8) - 1);
+		
+		int[][] v0 = values[a0];
+		if (v0 == null)
+			values[a0] = v0 = new int[1 << 8][];
+		
+		int[] v1 = v0[a1];
+		if (v1 == null)
+			v0[a1] = v1 = new int[1 << 8];
+		
+		v1[a2] = val;
 	}
 	
 }
