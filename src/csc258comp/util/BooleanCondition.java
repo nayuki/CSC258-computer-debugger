63777acbd35e07cc390f961730675565c1609207
Nayuki Minase <nayuki@eigenstate.org>
2010-09-08 20:31:40 +0000

    Added BooleanCondition class for synchronization.

---
+++ src/csc258comp/util/BooleanCondition.java
@@ -0,0 +1,34 @@
+package csc258comp.util;
+
+
+public final class BooleanCondition {
+	
+	private boolean value;
+	
+	
+	
+	public BooleanCondition(boolean val) {
+		value = val;
+	}
+	
+	
+	
+	public synchronized boolean getValue() {
+		return value;
+	}
+	
+	
+	public synchronized void setValue(boolean val) {
+		if (value != val) {
+			value = val;
+			notifyAll();
+		}
+	}
+	
+	
+	public synchronized void waitForValue(boolean val) throws InterruptedException {
+		while (value != val)
+			wait();
+	}
+	
+}
