cce126316d4cf28696df0a9ecd555139fa980214
Nayuki Minase <nayuki@eigenstate.org>
2010-09-08 20:17:43 +0000

    Updated test code to use new constructor.

--- tests/csc258comp/compiler/DataTestUtils.java
+++ tests/csc258comp/compiler/DataTestUtils.java
@@ -3,18 +3,11 @@ package csc258comp.compiler;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 
 final class DataTestUtils {
 	
 	public static Fragment compile(String code) {
-		List<String> lines = new ArrayList<String>();
-		Collections.addAll(lines, code.split("\n"));
-		
-		SourceCode source = new SourceCode(lines);
+		SourceCode source = new SourceCode(code);
 		return MyCompiler.compile(source);
 	}
 	
@@ -33,10 +26,7 @@ final class DataTestUtils {
 	
 	
 	public static void testInvalid(String code) throws CompilerException {
-		List<String> lines = new ArrayList<String>();
-		Collections.addAll(lines, code.split("\n"));
-		
-		SourceCode source = new SourceCode(lines);
+		SourceCode source = new SourceCode(code);
 		MyCompiler.compile(source);
 	}
 	
